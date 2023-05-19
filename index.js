946e228659f831300421daff9d12e1eea2df7b0e
Nitish Rajbongshi <nitishrajbongshi@gmail.com>
2023-05-19 12:46:00 +0530

    Make the public folder static. Add css file also

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Import the express
+// Import the modules
 const express = require("express");
+const path = require("path");
 
 // Initialise the app using express
 const app = express();
@@ -8,9 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // creating the routes
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
-});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+// Better way to do that is to make the public folder as static
+// It will automatically search for file in the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen to the PORT
 app.listen(PORT, () => {
